61bd45dfb5dd7786c39107364b757d02f558f6db
edis <61211582+edisedis777@users.noreply.github.com>
2025-03-10 10:49:56 +0100

    Update ingestion_dag.py

--- airflow/dags/ingestion_dag.py
+++ airflow/dags/ingestion_dag.py
@@ -1,1 +1,14 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
 
+with DAG(
+    dag_id='ingestion_dag',
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+) as dag:
+    task_echo = BashOperator(
+        task_id='echo_hello',
+        bash_command='echo "Hello, Airflow!"',
+    )
